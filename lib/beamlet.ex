1fe383f5669b65d1fd877d881c88f0174a0ffb1a
aaronrussell <124721263+aaronrussell@users.noreply.github.com>
2026-09-13 17:42:49 +0100

    remove hello world boilerplate

--- lib/beamlet.ex
+++ lib/beamlet.ex
@@ -2,17 +2,4 @@ defmodule Beamlet do
   @moduledoc """
   Documentation for `Beamlet`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Beamlet.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/beamlet_test.exs
+++ test/beamlet_test.exs
@@ -1,8 +1,4 @@
 defmodule BeamletTest do
   use ExUnit.Case
   doctest Beamlet
-
-  test "greets the world" do
-    assert Beamlet.hello() == :world
-  end
 end
